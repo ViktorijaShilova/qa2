be644ffaac8fdfb67f3b4bc53af2ed3140f05c2e
ViktorijaShilova <viktorija.shilova@gmail.com>
2018-12-09 14:49:24 +0200

    ingredient page

--- src/test/java/horoscope/pages/HoroscopePage.java
+++ src/test/java/horoscope/pages/HoroscopePage.java
@@ -1,4 +1,14 @@
 package horoscope.pages;
 
+import food.pages.BaseFunc;
+import org.openqa.selenium.By;
+
 public class HoroscopePage {
+    BaseFunc baseFunc;
+
+    public HoroscopePage(BaseFunc baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+
+    private final By SIGNS = By.xpath(".//div[@class = 'hrsc-list-title']");
 }
