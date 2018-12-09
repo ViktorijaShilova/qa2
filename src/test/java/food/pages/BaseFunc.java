52d55921059097414bedf0ef0a0444b0791212de
ViktorijaShilova <viktorija.shilova@gmail.com>
2018-12-09 14:57:14 +0200

    ingredient page

--- src/test/java/food/pages/BaseFunc.java
+++ src/test/java/food/pages/BaseFunc.java
@@ -1,4 +1,49 @@
 package food.pages;
 
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
 public class BaseFunc {
+
+    WebDriver browser;
+
+    public BaseFunc() {
+        System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
+        browser = new FirefoxDriver();
+        browser.manage().window().maximize();
+    }
+
+    public void goToUrl(String url) {
+        if (!url.contains("http://") && !url.contains("https://")) {
+            url = "http://" + url;
+        }
+
+        browser.get(url);
+    }
+
+
+
+    public List<WebElement> getElements(By locator) {
+        Assertions.assertFalse(browser.findElements(locator).isEmpty(), "There is no elements on page");
+        return browser.findElements(locator);
+    }
+
+    public WebElement getElement(By locator) {
+        Assertions.assertFalse(browser.findElements(locator).isEmpty(), "There is no element");
+        return browser.findElement(locator);
+    }
+
+//    public void waitForElement(By locator) {
+//        WebDriverWait wait = new WebDriverWait(browser, 10);
+//        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+//    }
+
 }
