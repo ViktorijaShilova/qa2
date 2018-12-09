0ff5885709510547f2dc78e29a44c9b7bbd1d7e2
ViktorijaShilova <viktorija.shilova@gmail.com>
2018-12-09 14:53:22 +0200

    ingredient page

--- src/test/java/food/pages/IngredientPage.java
+++ src/test/java/food/pages/IngredientPage.java
@@ -1,4 +1,21 @@
 package food.pages;
 
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IngredientPage {
+    BaseFunc baseFunc;
+
+    private final By INGR_RECIPES = By.xpath(".//h1[@class = 'headline-title']");
+
+    public IngredientPage (BaseFunc baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+
+//     Assertions.assertEquals(names.get(i), mobile_names.get(i), "Article" + i + "not right");
+
 }
