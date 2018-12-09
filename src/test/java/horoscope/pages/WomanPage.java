f08e385fe29ccdf3e31b3aca41ed650fd055bbea
ViktorijaShilova <viktorija.shilova@gmail.com>
2018-12-09 14:50:03 +0200

    ingredient page

--- src/test/java/horoscope/pages/WomanPage.java
+++ src/test/java/horoscope/pages/WomanPage.java
@@ -1,4 +1,32 @@
 package horoscope.pages;
 
+import food.pages.BaseFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class WomanPage {
+    BaseFunc baseFunc;
+
+    private final By TABS = By.xpath(".//h1[contains(@class, 'headline-title')]");
+
+    public WomanPage(BaseFunc baseFunc) {
+
+        this.baseFunc = baseFunc;
+    }
+
+    public HoroscopePage getTab(String name) {
+        List<WebElement> tabs = baseFunc.getElements(TABS);
+        for (WebElement tab : tabs) {
+            if (tab.findElement(TABS).getText().equals(name)) {
+                tab.click();
+                return new HoroscopePage(baseFunc);
+            }
+        }
+        return null;
+    }
+
+
+
 }
