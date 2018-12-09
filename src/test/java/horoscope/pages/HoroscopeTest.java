d30096f3cbd4c16711d809e669a396c7bb63e7bf
ViktorijaShilova <viktorija.shilova@gmail.com>
2018-12-09 14:49:43 +0200

    ingredient page

--- src/test/java/horoscope/pages/HoroscopeTest.java
+++ src/test/java/horoscope/pages/HoroscopeTest.java
@@ -1,4 +1,23 @@
 package horoscope.pages;
 
-public class HoroscopeTest {
-}
+import food.pages.BaseFunc;
+import food.pages.HomePage;
+import org.junit.jupiter.api.Test;
+
+    public class HoroscopeTest {
+
+        private BaseFunc baseFunc = new BaseFunc();
+        private final String HOME_PAGE = "rus.delfi.lv";
+
+        @Test
+        public void HoroscopeTest() {
+            baseFunc.goToUrl(HOME_PAGE);
+            food.pages.HomePage homePage = new HomePage(baseFunc);
+            homePage.CloseCookie();
+            WomanPage womanPage = homePage.getItemByName("Woman");
+            HoroscopePage horoscopePage = womanPage.getTab("Гороскоп");
+
+        }
+    }
+
+
