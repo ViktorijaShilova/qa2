f485cd428fe71b62884fd5e2b22932845311a584
ViktorijaShilova <viktorija.shilova@gmail.com>
2018-12-09 14:54:07 +0200

    ingredient page

--- src/test/java/food/pages/FoodTest.java
+++ src/test/java/food/pages/FoodTest.java
@@ -1,4 +1,19 @@
 package food.pages;
 
+import org.junit.jupiter.api.Test;
+
 public class FoodTest {
+
+    private BaseFunc baseFunc = new BaseFunc();
+    private final String HOME_PAGE = "rus.delfi.lv";
+
+    @Test
+    public void FoodTest() {
+        baseFunc.goToUrl(HOME_PAGE);
+        HomePage homePage = new HomePage(baseFunc);
+        homePage.CloseCookie();
+        FoodPage foodPage = homePage.getItemByName("Еда");
+        RecipePage recipePage = foodPage.getRecipe("Острый соус из апельсинов");
+
+    }
 }
